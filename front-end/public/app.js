0b61f9ee17fdb71e9178bcd5e14fb95d7486b296
Brian Ortiz <b.ortiz.a95@gmail.com>
2022-11-10 00:43:41 -0500

    I can finally go to bed

--- front-end/public/app.js
+++ front-end/public/app.js
@@ -1,13 +1,35 @@
-console.log("something");
-
 const carsUL = document.getElementById("cars");
-fetch("http://localhost:3000/api/automobiles/cars")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((element) => {
-      const carList = document.createElement("li");
-      console.log(element);
-      carList.innerText = element.model;
-      carsUL.appendChild(carList);
+const button = document.getElementById("carsButton");
+const clearButton = document.getElementById("carsClear");
+const container = document.getElementById("carContainer");
+const body = document.getElementById("body");
+carsUL.setAttribute("id", "outList");
+
+function getAllCars() {
+  fetch("http://localhost:3000/api/automobiles/cars")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((element) => {
+        console.log(element);
+        const carList = document.createElement("li");
+        const carDiv = document.createElement("div");
+        carDiv.setAttribute("id", "carDiv");
+        carsUL.appendChild(carList);
+        carList.appendChild(carDiv);
+        carDiv.innerHTML = `Model: ${element.model}<br>Color: ${element.color}<br>
+        Year: ${element.year}<br>MSRP: ${element.MSRP}`;
+      });
     });
-  });
+}
+
+// getAllCars();
+
+button.addEventListener("click", function () {
+  body.appendChild(container);
+  getAllCars();
+});
+
+clearButton.addEventListener("click", function () {
+  container.hidden = true;
+  container.remove();
+});
